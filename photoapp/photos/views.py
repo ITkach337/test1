8fdfc0937984435ada2dd94e48df5fa52999d89b
Tkachenko Ivan <ivancar1573@gmail.com>
2022-08-02 21:15:22 +0300

    1

---
+++ photoapp/photos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import photos #import photos model
+
+def index(request):
+    # imports photos and save it in database
+    photo = photos.objects.all()
+    # adding context
+    ctx = {'photo':photo}
+    return render(request, 'index.html', ctx)
